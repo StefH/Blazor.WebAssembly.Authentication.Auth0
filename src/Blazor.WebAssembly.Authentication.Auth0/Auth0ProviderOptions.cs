f5544c3e30c05cf46c6c0301d62f8546b986faba
Robert McLaws <robert@nimbleapps.cloud>
2021-09-19 14:59:00 -0400

    .NET 6.0 Support (fixes #7): (#8)
    - Updated Auth0ProviderOptions to provide compatibility with .NET 6 and later without breaking .NET 5 and earlier.
    - Added documentation comments based on the .NET 6.0 code.
    - Updated the solution to better target specific platforms for compatibility.

--- src/Blazor.WebAssembly.Authentication.Auth0/Auth0ProviderOptions.cs
+++ src/Blazor.WebAssembly.Authentication.Auth0/Auth0ProviderOptions.cs
@@ -1,36 +1,57 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
 {
+
+    /// <summary>
+    /// Enhances the <see cref="OidcProviderOptions"/> with Auth0-specific configuration.
+    /// </summary>
     public class Auth0ProviderOptions : OidcProviderOptions
     {
+
         private const string AudienceKey = "audience";
 
         /// <summary>
         /// Sets the audience of the application.
         /// </summary>
-        public string? Audience { get; set; }
+        public string Audience
+        {
+            get => AdditionalProviderParameters.ContainsKey(AudienceKey) ? AdditionalProviderParameters[AudienceKey] : "";
+            set => AdditionalProviderParameters[AudienceKey] = value;
+        }
 
         /// <summary>
-        /// Extra Query Parameters
-        /// 
-        /// See https://github.com/IdentityModel/oidc-client-js/blob/dev/src/OidcClient.js#L65
+        /// Gets or sets the additional provider parameters to use on the authorization flow.
         /// </summary>
-        [JsonPropertyName("extraQueryParams")]
+        /// <remarks>
+        /// These parameters are for the IdP and not for the application. Using those parameters in the application in any way on the login callback will likely introduce security issues as they should be treated as untrusted input.
+        /// </remarks>
+        [JsonIgnore]
+        [Obsolete("This property is called 'AdditionalProviderParameters' in OidcProviderOptions on .NET 6.0. For future compatibility, " +
+            "we've left this property as a pointer to the new one. It will be removed in the next major version.", error: false)]
         public IDictionary<string, string> ExtraQueryParams
         {
-            get
-            {
-                var dictionary = new Dictionary<string, string>();
-                if (!string.IsNullOrWhiteSpace(Audience))
-                {
-                    dictionary.Add(AudienceKey, Audience);
-                }
-
-                return dictionary;
-            }
+            get => AdditionalProviderParameters;
         }
+
+#if !NET6_0_OR_GREATER
+
+        /// <summary>
+        /// Gets or sets the additional provider parameters to use on the authorization flow.
+        /// </summary>
+        /// <remarks>
+        /// See https://github.com/IdentityModel/oidc-client-js/blob/dev/src/OidcClient.js#L65.
+        /// This property is already available in .NET 6.0, so we're hiding it on .NET 6 and later so it won't break the deserializer.
+        /// These parameters are for the IdP and not for the application. Using those parameters in the application in any way on the login callback will likely introduce security issues as they should be treated as untrusted input.
+        /// </remarks>
+        [JsonPropertyName("extraQueryParams")]
+        public IDictionary<string, string> AdditionalProviderParameters { get; set; } = new Dictionary<string, string>();
+
+#endif
+
     }
+
 }
