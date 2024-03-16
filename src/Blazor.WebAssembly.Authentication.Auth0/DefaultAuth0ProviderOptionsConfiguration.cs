f63ff8cac9e57cf486a199e971f2587cb22c4dfc
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-03-16 11:05:24 +0100

    Add .NET 8

--- src/Blazor.WebAssembly.Authentication.Auth0/DefaultAuth0ProviderOptionsConfiguration.cs
+++ src/Blazor.WebAssembly.Authentication.Auth0/DefaultAuth0ProviderOptionsConfiguration.cs
@@ -2,41 +2,40 @@
 using Microsoft.Extensions.Options;
 
 // ReSharper disable once CheckNamespace
-namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication
+namespace Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+internal class DefaultAuth0ProviderOptionsConfiguration : IPostConfigureOptions<RemoteAuthenticationOptions<Auth0ProviderOptions>>
 {
-    internal class DefaultAuth0ProviderOptionsConfiguration : IPostConfigureOptions<RemoteAuthenticationOptions<Auth0ProviderOptions>>
+    private readonly NavigationManager _navigationManager;
+
+    public DefaultAuth0ProviderOptionsConfiguration(NavigationManager navigationManager) => _navigationManager = navigationManager;
+
+    public void Configure(RemoteAuthenticationOptions<Auth0ProviderOptions> options)
     {
-        private readonly NavigationManager _navigationManager;
+        options.UserOptions.AuthenticationType ??= options.ProviderOptions.ClientId;
 
-        public DefaultAuth0ProviderOptionsConfiguration(NavigationManager navigationManager) => _navigationManager = navigationManager;
-
-        public void Configure(RemoteAuthenticationOptions<Auth0ProviderOptions> options)
+        var redirectUri = options.ProviderOptions.RedirectUri;
+        if (redirectUri == null || !Uri.TryCreate(redirectUri, UriKind.Absolute, out _))
         {
-            options.UserOptions.AuthenticationType ??= options.ProviderOptions.ClientId;
-
-            var redirectUri = options.ProviderOptions.RedirectUri;
-            if (redirectUri == null || !Uri.TryCreate(redirectUri, UriKind.Absolute, out _))
-            {
-                redirectUri ??= "authentication/login-callback";
-                options.ProviderOptions.RedirectUri = _navigationManager
-                    .ToAbsoluteUri(redirectUri).AbsoluteUri;
-            }
-
-            var logoutUri = options.ProviderOptions.PostLogoutRedirectUri;
-            if (logoutUri == null || !Uri.TryCreate(logoutUri, UriKind.Absolute, out _))
-            {
-                logoutUri ??= "authentication/logout-callback";
-                options.ProviderOptions.PostLogoutRedirectUri = _navigationManager
-                    .ToAbsoluteUri(logoutUri).AbsoluteUri;
-            }
+            redirectUri ??= "authentication/login-callback";
+            options.ProviderOptions.RedirectUri = _navigationManager
+                .ToAbsoluteUri(redirectUri).AbsoluteUri;
         }
 
-        public void PostConfigure(string name, RemoteAuthenticationOptions<Auth0ProviderOptions> options)
+        var logoutUri = options.ProviderOptions.PostLogoutRedirectUri;
+        if (logoutUri == null || !Uri.TryCreate(logoutUri, UriKind.Absolute, out _))
         {
-            if (string.Equals(name, Options.DefaultName))
-            {
-                Configure(options);
-            }
+            logoutUri ??= "authentication/logout-callback";
+            options.ProviderOptions.PostLogoutRedirectUri = _navigationManager
+                .ToAbsoluteUri(logoutUri).AbsoluteUri;
+        }
+    }
+
+    public void PostConfigure(string? name, RemoteAuthenticationOptions<Auth0ProviderOptions> options)
+    {
+        if (string.Equals(name, Options.DefaultName))
+        {
+            Configure(options);
         }
     }
 }
